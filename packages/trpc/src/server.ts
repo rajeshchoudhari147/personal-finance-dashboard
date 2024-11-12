fb942316e3ee25e799cb5c3ff1974d4708637c1c
Rajesh Choudhari <48985833+rajeshchoudhari147@users.noreply.github.com>
2024-11-12 22:11:00 +1100

    Initialize Turbo monorepo

---
+++ packages/trpc/src/server.ts
@@ -0,0 +1,13 @@
+import { initTRPC } from '@trpc/server';
+import superjson from 'superjson';
+
+const t = initTRPC.create({
+  transformer: superjson,
+});
+
+export const router = t.router;
+export const publicProcedure = t.procedure;
+
+export type AppRouter = typeof appRouter;
+
+export const appRouter = router({});
